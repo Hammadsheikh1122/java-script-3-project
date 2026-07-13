89cd8598cd95eef5749856f266b34eb3defb6863
Hammadsheikh1122 <hammadsheikhsheikh6@gmail.com>
2026-07-13 06:22:05 -0800

    first commit

---
+++ app.js
@@ -0,0 +1,43 @@
+//Task 1//
+let  age = 16;
+console.log(age)
+let month = 12;
+console.log(month)
+let week = 100;
+console.log(week)
+let price = 4 ;
+console.log(price)
+let year = 30;
+console.log(year)
+
+let task ="Task 2"
+console.log(task)
+//illegel variable//
+// let let = "illegal";
+// console.log(let)
+
+// const const = "illegel";
+// console.log(const)
+
+// var var = "illegel"
+// console.log(var)
+
+//legel variable//
+var name = "Hammad";
+console.log(name);
+
+let fatherName = "Amin";
+console.log(fatherName);
+
+const days = 16;
+console.log(days)
+
+let task = "task3"
+console.log(task)
+let a = 100;
+let b = 20;
+console.log(a+b); //Addition//
+console.log(a-b); //substraction//
+console.log(a*b); //multiplication//
+console.log(a/b); //devidion//
+console.log(a%b); //module//
